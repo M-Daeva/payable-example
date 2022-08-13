0b076a7b9992b162a77a681750c91513257355c1
M. Daeva <embedmad@gmail.com>
2022-08-13 16:30:45 +0300

    added unit tests

--- src/contract.rs
+++ src/contract.rs
@@ -2,6 +2,7 @@
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{
     from_binary, to_binary, Binary, Coin, Deps, DepsMut, Env, MessageInfo, Response, StdResult,
+    Uint128,
 };
 use cw2::set_contract_version;
 use cw20::Cw20ReceiveMsg;
@@ -25,10 +26,11 @@ pub fn instantiate(
     let cw20 = deps.api.addr_validate(&msg.staking_token)?;
 
     let state = State {
-        owner: owner.clone(),
-        cw20: cw20.clone(),
+        owner: owner.clone(), // smart contract instantiator
+        cw20: cw20.clone(),   // cw20 token contract address
         staked: Coin {
-            ..Default::default()
+            amount: Uint128::new(0),
+            denom: String::from("ustake"),
         },
     };
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
@@ -64,22 +66,19 @@ pub fn receive_cw20(
 ) -> Result<Response, ContractError> {
     // only asset contract can execute this message
     let config: State = STATE.load(deps.storage)?;
-    let addr = deps.api.addr_validate(info.sender.as_str())?;
-    if addr != config.cw20 {
+    let sender = deps.api.addr_validate(info.sender.as_str())?;
+    if sender != config.owner {
         return Err(ContractError::Unauthorized {});
     }
 
     match from_binary(&cw20_msg.msg) {
         Ok(Cw20HookMsg::StakeVotingTokens {}) => {
-            // Homework??
-            let coin = &info.funds[0];
-
             let state = State {
                 owner: config.owner,
                 cw20: config.cw20,
                 staked: Coin {
-                    amount: coin.amount,
-                    denom: coin.denom.to_string(),
+                    amount: cw20_msg.amount,
+                    denom: config.staked.denom,
                 },
             };
             STATE.save(deps.storage, &state)?;
@@ -114,25 +113,100 @@ fn query_count(deps: Deps) -> StdResult<GetAmountResponse> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
-    use cosmwasm_std::{coins, from_binary};
+    use crate::contract::{execute, instantiate, query};
+    use crate::msg::{Cw20HookMsg, ExecuteMsg, GetAmountResponse, InstantiateMsg, QueryMsg};
+    use crate::ContractError;
+    use cosmwasm_std::testing::{
+        mock_dependencies, mock_env, mock_info, MockApi, MockQuerier, MockStorage,
+    };
+    use cosmwasm_std::{
+        attr, coins, from_binary, Empty, Env, MessageInfo, OwnedDeps, Response, Uint128,
+    };
+
+    pub const OWNER_ADDR: &str = "addr1";
+    pub const TOKEN_ADDR: &str = "addr2";
+    pub const ADDR3: &str = "addr3";
+
+    type Instance = (
+        OwnedDeps<MockStorage, MockApi, MockQuerier, Empty>,
+        Env,
+        MessageInfo,
+        Result<Response, ContractError>,
+    );
+
+    fn get_instance(staking_token: String, addr: &str) -> Instance {
+        let mut deps = mock_dependencies();
+        let env = mock_env();
+        let info = mock_info(addr, &[]);
+        let msg = InstantiateMsg { staking_token };
+
+        let res = instantiate(deps.as_mut(), env.clone(), info.clone(), msg);
+        (deps, env, info, res)
+    }
 
     #[test]
-    fn proper_initialization() {
-        let mut deps = mock_dependencies();
+    fn test_init() {
+        let (_, _, _, res) = get_instance(TOKEN_ADDR.to_string(), OWNER_ADDR);
 
-        let msg = InstantiateMsg {
-            staking_token: "".to_string(),
-        };
-        let info = mock_info("creator", &coins(1000, "earth"));
+        assert_eq!(
+            res.unwrap().attributes,
+            vec![
+                attr("method", "instantiate"),
+                attr("owner", OWNER_ADDR),
+                attr("cw20", TOKEN_ADDR),
+                attr("staked_amount", "0"),
+                attr("staked_denom", "ustake")
+            ]
+        );
+    }
 
-        // we can just call .unwrap() to assert this was a success
-        let res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
-        assert_eq!(0, res.messages.len());
+    #[test]
+    fn test_execute() {
+        let (mut deps, env, info, res) = get_instance(TOKEN_ADDR.to_string(), OWNER_ADDR);
 
-        // it worked, let's query the state
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: GetCountResponse = from_binary(&res).unwrap();
-        assert_eq!(17, value.count);
+        let msg = ExecuteMsg::Receive(Cw20ReceiveMsg {
+            amount: Uint128::new(420),
+            sender: String::from(OWNER_ADDR),
+            msg: to_binary(&Cw20HookMsg::StakeVotingTokens {}).unwrap(),
+        });
+
+        let res2 = execute(deps.as_mut(), env, info, msg);
+
+        assert_eq!(
+            res2.unwrap().attributes,
+            vec![
+                attr("method", "receive_cw20"),
+                attr("owner", OWNER_ADDR),
+                attr("cw20", TOKEN_ADDR),
+                attr("staked_amount", "420"),
+                attr("staked_denom", "ustake")
+            ]
+        );
+    }
+
+    #[test]
+    fn test_query() {
+        let (mut deps, env, info, res) = get_instance(TOKEN_ADDR.to_string(), OWNER_ADDR);
+
+        let msg = ExecuteMsg::Receive(Cw20ReceiveMsg {
+            amount: Uint128::new(420),
+            sender: String::from(OWNER_ADDR),
+            msg: to_binary(&Cw20HookMsg::StakeVotingTokens {}).unwrap(),
+        });
+
+        execute(deps.as_mut(), env.clone(), info, msg);
+
+        let msg = QueryMsg::GetAmount {};
+
+        let bin = query(deps.as_ref(), env, msg).unwrap();
+
+        let res = from_binary::<GetAmountResponse>(&bin).unwrap();
+
+        assert_eq!(
+            res,
+            GetAmountResponse {
+                amount: Uint128::new(420),
+            }
+        );
     }
 }
